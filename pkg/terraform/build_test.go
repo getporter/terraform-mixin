2f33fbc654a2684197449d08f5e1c47e30967192
Vaughn Dice <vadice@microsoft.com>
2019-12-02 18:07:22 -0700

    feat(build.go): run terraform init (sans backend setup) on build
    Signed-off-by: Vaughn Dice <vadice@microsoft.com>

--- pkg/terraform/build_test.go
+++ pkg/terraform/build_test.go
@@ -16,7 +16,10 @@ func TestMixin_Build(t *testing.T) {
 	wantOutput := `ENV TERRAFORM_VERSION=0.11.11
 RUN apt-get update && apt-get install -y wget unzip && \
  wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip && \
- unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /usr/bin`
+ unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /usr/bin
+COPY . $BUNDLE_DIR
+RUN cd /cnab/app/terraform && terraform init -backend=false
+`
 
 	gotOutput := m.TestContext.GetOutput()
 	assert.Equal(t, wantOutput, gotOutput)
