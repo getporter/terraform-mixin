f5a58f17266375f5936c22d27b179fb5e1be27cb
Christopher Crone <christopher.crone@docker.com>
2020-07-24 12:30:00 +0200

    build: Add ability to set version
    Signed-off-by: Christopher Crone <christopher.crone@docker.com>

--- pkg/terraform/build.go
+++ pkg/terraform/build.go
@@ -2,9 +2,11 @@ package terraform
 
 import (
 	"fmt"
+
+	"get.porter.sh/porter/pkg/exec/builder"
+	"github.com/ghodss/yaml"
 )
 
-const terraformClientVersion = "0.12.17"
 const dockerfileLines = `ENV TERRAFORM_VERSION=%s
 RUN apt-get update && apt-get install -y wget unzip && \
  wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip && \
@@ -13,7 +15,33 @@ COPY . $BUNDLE_DIR
 RUN cd %s && terraform init -backend=false
 `
 
+// BuildInput represents stdin passed to the mixin for the build command.
+type BuildInput struct {
+	Config MixinConfig
+}
+
+// MixinConfig represents configuration that can be set on the terraform mixin in porter.yaml
+// mixins:
+// - terraform:
+//	  version: 0.12.17
+type MixinConfig struct {
+	ClientVersion string `yaml:"clientVersion,omitempty"`
+}
+
 func (m *Mixin) Build() error {
-	fmt.Fprintf(m.Out, dockerfileLines, terraformClientVersion, m.WorkingDir)
+	var input BuildInput
+	err := builder.LoadAction(m.Context, "", func(contents []byte) (interface{}, error) {
+		err := yaml.Unmarshal(contents, &input)
+		return &input, err
+	})
+	if err != nil {
+		return err
+	}
+
+	if input.Config.ClientVersion != "" {
+		m.TerraformVersion = input.Config.ClientVersion
+	}
+
+	fmt.Fprintf(m.Out, dockerfileLines, m.TerraformVersion, m.WorkingDir)
 	return nil
 }
--- pkg/terraform/build_test.go
+++ pkg/terraform/build_test.go
@@ -1,19 +1,16 @@
 package terraform
 
 import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func TestMixin_Build(t *testing.T) {
-	m := NewTestMixin(t)
-
-	err := m.Build()
-	require.NoError(t, err)
-
-	wantOutput := `ENV TERRAFORM_VERSION=0.12.17
+const buildOutputTemplate = `ENV TERRAFORM_VERSION=%s
 RUN apt-get update && apt-get install -y wget unzip && \
  wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip && \
  unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /usr/bin
@@ -21,6 +18,31 @@ COPY . $BUNDLE_DIR
 RUN cd /cnab/app/terraform && terraform init -backend=false
 `
 
-	gotOutput := m.TestContext.GetOutput()
-	assert.Equal(t, wantOutput, gotOutput)
+func TestMixin_Build(t *testing.T) {
+	t.Run("build with the default Terraform version", func(t *testing.T) {
+		m := NewTestMixin(t)
+
+		err := m.Build()
+		require.NoError(t, err)
+
+		wantOutput := fmt.Sprintf(buildOutputTemplate, "0.12.17")
+
+		gotOutput := m.TestContext.GetOutput()
+		assert.Equal(t, wantOutput, gotOutput)
+	})
+
+	t.Run("build with custom Terrafrom version", func(t *testing.T) {
+		b, err := ioutil.ReadFile("testdata/build-input-with-version.yaml")
+		require.NoError(t, err)
+
+		m := NewTestMixin(t)
+		m.In = bytes.NewReader(b)
+		err = m.Build()
+		require.NoError(t, err)
+
+		wantOutput := fmt.Sprintf(buildOutputTemplate, "0.13.0-rc1")
+
+		gotOutput := m.TestContext.GetOutput()
+		assert.Equal(t, wantOutput, gotOutput)
+	})
 }
